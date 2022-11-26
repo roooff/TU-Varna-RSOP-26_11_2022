2c918c6b71f61ffb7aacc1250d2bb22eb3bd8584
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2022-11-27 00:18:00 +0200

    Update Program.cs

--- XNiz/Program.cs
+++ XNiz/Program.cs
@@ -9,32 +9,83 @@ namespace XNiz
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        //char[] alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        //    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        //int test = int.Parse(Console.ReadLine());
+        //var alphabet = new List<char>(alpha);
+        //var nums = new List<int>();
+        //nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        //.Select(int.Parse).ToList();
+
+        //var sb = new StringBuilder();
+        //string str = sb.ToString();
+        //for (int i = 0; i < test; i++)
+        //{
+        //    int chars = nums[0];
+        //    int place = nums[1];
+
+        //    for (int j = 0; j < chars; j++)
+        //    {
+        //        sb.Append(alpha[j]);
+        //    }
+
+        // C# program to print all
+
+        private class GFG
         {
-            char[] alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+            /**
+            * permutation function
+            * @param str string to
+            calculate permutation for
+            * @param l starting index
+            * @param r end index
+            */
+            private string str = string.Empty;
 
-            var alphabet = new List<char>(alpha);
-            var nums = new List<int>();
-
-            int test = int.Parse(Console.ReadLine());
-            nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
-            .Select(int.Parse).ToList();
-            var word = new List<string>();
-            int counter = 0;
-            var sb = new StringBuilder();
-            for (int i = 0; i < test; i++)
+            private static void permute(string str, int l, int r)
             {
-                int chars = nums[0];
-                int place = nums[1];
-
-                for (int j = 0; j < chars; j++)
+                List<string> list = new List<string>();
+                if (l == r)
+                    list.Add(str);
+                else
                 {
-                    sb.Append(alpha[j]);
-                    counter++;
+                    for (int i = l; i <= r; i++)
+                    {
+                        str = swap(str, l, i);
+                        permute(str, l + 1, r);
+                        str = swap(str, l, i);
+                    }
                 }
             }
-            Console.WriteLine(sb);
+
+            /**
+            * Swap Characters at position
+            * @param a string value
+            * @param i position 1
+            * @param j position 2
+            * @return swapped string
+            */
+
+            public static String swap(String a, int i, int j)
+            {
+                char temp;
+                char[] charArray = a.ToCharArray();
+                temp = charArray[i];
+                charArray[i] = charArray[j];
+                charArray[j] = temp;
+                string s = new string(charArray);
+                return s;
+            }
+
+            // Driver Code
+            public static void Main()
+            {
+                String str = "ABC";
+                int n = str.Length;
+                permute(str, 0, n - 1);
+            }
         }
+
+        // This code is contributed by mits
     }
 }

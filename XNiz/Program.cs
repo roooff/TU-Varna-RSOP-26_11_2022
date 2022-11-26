bcb9d7cb0541548bbcaf6ba36284e4dbe856ee3d
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2022-11-26 14:30:58 +0200

    New projects

---
+++ XNiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XNiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

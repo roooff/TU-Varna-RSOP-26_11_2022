e9167c82cd3b0eecf522bea35713bde18ed43a80
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2022-11-26 12:06:25 +0200

    Update Program.cs

--- Рождена дата/Program.cs
+++ Рождена дата/Program.cs
@@ -7,8 +7,10 @@ namespace Рождена_дата
     {
         private static void Main(string[] args)
         {
-            int[] birthdayInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            int[] currentYearInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] birthdayInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int[] currentYearInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
             var birthday = new DateTime(2022, birthdayInput[1], birthdayInput[0]);
             var currentDate = new DateTime(currentYearInput[2], currentYearInput[1], currentYearInput[0]);
             TimeSpan diff = birthday - currentDate;
